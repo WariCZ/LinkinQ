bd1732125b5301fb0ff215ecf375341dcd233d3c
Jakub Vařeka <jakub.vareka@physter.com>
2025-04-15 18:34:14 +0200

    WIP - sucess app

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./loc/index"; // Načtení konfigurace i18n
+import "./loc/index";
 import ModalContainer from "./components/Modal/ModalContainer";
 import "react-tabs/style/react-tabs.css";
 
 const createLinkinqClient = () => {
-  console.log("XXXX");
-  debugger;
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <>
       <App />
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,5 @@
+const flowbite = require("flowbite-react/tailwind");
+
 export { Linkinq } from "./app";
+
+export { flowbite };
